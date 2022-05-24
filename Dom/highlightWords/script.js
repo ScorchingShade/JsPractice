1cf074f47fbff615f399d6c9e4b0b579401bc970
ScorchingShade <ankush.sharma.con@hotstar.com>
2022-05-25 02:11:40 +0530

    added DOM practice

---
+++ Dom/highlightWords/script.js
@@ -0,0 +1,8 @@
+// Highlight all of the words over 8 characters long in the paragraph text (with a yellow bg)
+
+
+const para = document.querySelector('p');
+
+para.innerHTML=para.innerHTML.split(" ").map((ele)=>ele.length>8?`<span style="background-color:yellow">${ele}</span>`:ele)
+
+console.log("para",para.innerHTML)
